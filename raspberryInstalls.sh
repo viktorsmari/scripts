57428a6553cdd224ee2f8b5ec4663b91544a846a
Viktor Smari <viktorsmari@gmail.com>
2015-09-16 22:34:50 +0200

    Add more prog to install list

--- raspberryInstalls.sh
+++ raspberryInstalls.sh
@@ -1,4 +1,10 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install munin tmux vim nmap htop
+sudo apt-get install tmux vim nmap htop
+echo "syntax on \n set nu" >> /home/pi/.vimrc
+echo "alias ll='ls -al'" >> /home/pi/.bashrc
 
+
+# Optional stuff
+#sudo apt-get install munin
+#sudo apt-get install zoneminder
