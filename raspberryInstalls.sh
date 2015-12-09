041599925468d35a301cfa144bf73530cd999479
Viktor Smári <viktorsmari@users.noreply.github.com>
2015-12-09 17:15:49 +0100

    Update raspberryInstalls.sh

--- raspberryInstalls.sh
+++ raspberryInstalls.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install tmux vim nmap htop
+
+# vim stuff
 echo "syntax on \n set nu" >> /home/pi/.vimrc
+
+# bash stuff
 echo "alias ll='ls -al'" >> /home/pi/.bashrc
-
-
-# Optional stuff
-#sudo apt-get install munin
-#sudo apt-get install zoneminder
+echo "alias l='ls -al'" >> /home/pi/.bashrc
+#echo "export LC_ALL=C" >> /home/pi/.bashrc
