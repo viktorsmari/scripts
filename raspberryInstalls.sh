ccc7559de5d1b86d3fd291e1f421b8c2fb13987b
Viktor Smári <viktorsmari@users.noreply.github.com>
2015-12-09 17:23:13 +0100

    Update raspberryInstalls.sh

--- raspberryInstalls.sh
+++ raspberryInstalls.sh
@@ -1,6 +1,8 @@
+# to run this do: curl https://raw.githubusercontent.com/viktorsmari/scripts/master/raspberryInstalls.sh| sh
+
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install tmux vim nmap htop
+sudo apt-get install tmux vim nmap htop -y
 
 # vim stuff
 echo "syntax on \n set nu" >> /home/pi/.vimrc
@@ -8,4 +10,4 @@ echo "syntax on \n set nu" >> /home/pi/.vimrc
 # bash stuff
 echo "alias ll='ls -al'" >> /home/pi/.bashrc
 echo "alias l='ls -al'" >> /home/pi/.bashrc
-#echo "export LC_ALL=C" >> /home/pi/.bashrc
+echo "export LC_ALL=C" >> /home/pi/.bashrc
