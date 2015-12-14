49dd8c555b7a637be7d2c49dacc3b83903496341
Viktor Smári <viktorsmari@users.noreply.github.com>
2015-12-14 15:07:52 +0100

    Update dweetDynamicIP.sh

--- dweetDynamicIP.sh
+++ dweetDynamicIP.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 curl -H "Content-Type: application/json" -XPOST https://dweet.io/dweet/for/viktorsmari -d '{"testIP":"'$(curl http://icanhazip.com)'"}' 
+
+#get dweet
+#curl -XGET https://dweet.io/get/latest/dweet/for/viktorsmari
